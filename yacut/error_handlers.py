935a19bcc59b105dd120c3ce75160d1fc707dcdb
Sabina <sabina_045@mail.ru>
2023-10-12 18:16:22 +0700

    Дз спринт 21

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -40,16 +40,3 @@ class InvalidAPIUsage(Exception):
 def invalid_api_usage(error):
 
     return jsonify(error.to_dict()), error.status_code
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    db.session.rollback()
-
-    return render_template('500.html'), 500
